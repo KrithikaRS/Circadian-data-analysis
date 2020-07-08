2ffb53415b03b9a1c1cb74496b1cb6e8cfc37e9a
Krithika Ramasamy <krithikars19@gmail.com>
2020-07-08 13:58:40 -0400

    Update phase_linearity_in_CYCLOPS_ordered_Gtex_data.py

--- phase_linearity_in_CYCLOPS_ordered_Gtex_data.py
+++ phase_linearity_in_CYCLOPS_ordered_Gtex_data.py
@@ -1,35 +1,16 @@
 import os, sys, string
 
-all_groups_file = "/Users/ram5ge/Desktop/GTEX_breast_v8/GTExv7CircAtlas_SmplPhases.csv"
-output_file = "/Users/ram5ge/Desktop/GTEX_breast_v8/Gtex_comp_orders_for_breast_donors_more_than_2_tissues.csv"
+all_groups_file = "/Users/ram5ge/Desktop/GTEX_breast_v8/GTExv7CircAtlas_SmplPhases_w_breast.csv"
+output_file = "/Users/ram5ge/Desktop/GTEX_breast_v8/Gtex_breast_ordering_from_adipose_donors2.csv"
 phase_diff_file = "/Users/ram5ge/Desktop/GTEX_breast_v8/CYCLOPS_order_phase_diff_btw_samples.txt"
 breast_groups_file = "/Users/ram5ge/Desktop/GTEX_breast_v8/groups.Gtex_Breast.txt"
 def phase_linearity():
     output = open(output_file, 'w')
-    #output.write("Donor"+','+"Aorta_sample"+','+"Aorta_Phase"+','+"Liver_sample"+','+"Liver_Phase"+'\n')
+    output.write("Donor"+','+"Breast_sample"+','+"Breast_Phase"+','+"Fat_sample"+','+"Fat_Phase"+'\n')
     sample_dict = {}
     donor_dict= {}
+    tissue_dict= {}
     phase_diff_dict = {}
-    for dlines in open(phase_diff_file, 'rU').readlines()[1:]:
-        ddata = dlines.rstrip()
-        pre_orer_data = string.split(ddata, '\t')
-        Donor = pre_orer_data[0]
-        phase_diff = float(pre_orer_data[4])
-        Tissues = pre_orer_data[6]
-        Tissues1 = string.split(Tissues, "|")
-        #print len(Tissues1)
-        #print Tissues#, Donor, phase_diff;sys.exit()
-        if phase_diff <4 and len(Tissues1)>=2:
-            phase_diff_dict[Donor] = (Tissues)
-    #print phase_diff_dict;sys.exit()
-    b_dict = {}
-    for blines in open(breast_groups_file, 'rU').readlines()[1:]:
-        bdata = blines.rstrip()
-        #print bdata;sys.exit()
-        b_samples =bdata
-        b_donor = string.split(bdata, '-')
-        b_donor = b_donor[0]+'-'+b_donor[1]
-        b_dict[b_donor] = (b_samples)
     #print b_dict;sys.exit()
     for lines in open(all_groups_file, 'rU').readlines()[1:]:
         data = lines.rstrip()
@@ -42,52 +23,74 @@ def phase_linearity():
         #print Sample;sys.exit()
         donor = string.split(Sample, '-')
         donor = donor[0]+'-'+donor[1]
-        try:donor_dict[donor].append (Sample,)
-        except Exception:donor_dict[donor] = [Sample]
-    comp_list=[]    
-    for p in phase_diff_dict:
-        if p in b_dict:
-            #print p
-            b = b_dict[p]
-            sam_id = donor_dict[p]
-            #print sam_id#;sys.exit()
-            for x in sam_id:
-                T = sample_dict[x]
-                #print T;sys.exit()
-                comp_list.append([p,b,x,T])
-    #print comp_list;sys.exit()
-    for m in comp_list:#(m, n, o) in comp_list:
-        output.write(str(m)[1:-1])
-        output.write('\n')
-    output.close()
-        
         #print tissue, Sample#, Ephase, #;sys.exit()
-        # if tissue == "Aorta" or tissue == "Liver":
-        #     #print Sample, tissue
-        #     donor = string.split(Sample, '-')
-        #     donor = donor[0]+'-'+donor[1]
-        #     try:donor_dict[donor].append (Sample,)
-        #     except Exception:donor_dict[donor] = [Sample]
-        #     sample_dict [Sample] = (Ephase)
+        if tissue == "Breast" or tissue == "Fat SQ" or tissue == "Fat Visceral":
+            #print Sample, tissue
+            donor = string.split(Sample, '-')
+            donor = donor[0]+'-'+donor[1]
+            try:donor_dict[donor].append (Sample,)
+            except Exception:donor_dict[donor] = [Sample]
+            sample_dict [Sample] = (Ephase)
+            try:tissue_dict[donor].append (tissue,)
+            except Exception:tissue_dict[donor] = [tissue]
     # print len(sample_dict)
-    # print len(donor_dict);sys.exit()
-    
-    # x =0
-    # for D in donor_dict:
-    #     if len(donor_dict[D]) ==2:
-    #         x+=1
-    #         #print x
-    #         #print D
-    #         S_Heart = donor_dict[D][0]
-    #         S_Liver = donor_dict[D][1]
-    #         Liver_Phase =sample_dict[S_Liver]
-    #         Heart_Phase = sample_dict[S_Heart]
-    #         output.write(D+','+S_Heart+','+Heart_Phase+','+S_Liver+','+Liver_Phase+'\n')
-    #         #print D, S_Heart, S_Liver;sys.exit()
-    # output.close()
+    # print len(tissue_dict)
+    #print tissue_dict;sys.exit()
+    x =0
+    y = 0
+    for D in donor_dict:
+        print D, str(tissue_dict[D])#[1:-1]#;sys.exit()
+        if len(donor_dict[D]) ==2:
+            if "Breast" and "Fat SQ" in tissue_dict[D] or "Breast" and "Fat Visceral"in tissue_dict[D]:
+                x+=1
+                tis = str(tissue_dict[D])[1:-1]
+                tis = string.replace(tis,',', '|')
+                print tis
+                S_Breast = donor_dict[D][0]
+                S_Fat = donor_dict[D][1]
+                Breast_Phase =sample_dict[S_Breast]
+                Fat_Phase = sample_dict[S_Fat]
+                #print D, S_Breast, Breast_Phase, S_Fat,Fat_Phase ;sys.exit()
+                output.write(D+','+S_Breast+','+Breast_Phase+','+S_Fat+','+tis+','+Fat_Phase+'\n')
+                
+                
+        elif len(donor_dict[D]) >2:
+            if "Breast" and "Fat SQ" in tissue_dict[D] or "Breast" and "Fat Visceral"in tissue_dict[D]:
+                y+=1
+                tis = str(tissue_dict[D])[1:-1]
+                tis = string.replace(tis,',', '|')
+                S_Breast = donor_dict[D][0]
+                S_Fat1 = donor_dict[D][1]
+                S_Fat2 = donor_dict[D][2]
+                S_Fat = S_Fat1+ "|"+S_Fat2
+                Breast_Phase =sample_dict[S_Breast]
+                Fat_Phase1 = float(sample_dict[S_Fat1])*3.8216
+                Fat_Phase2 = float(sample_dict[S_Fat2])*3.8216
+                Fat_phase = [Fat_Phase1,Fat_Phase2 ]
+                Max = max(Fat_phase)
+                Min = min(Fat_phase)
+                if Max >= 21 and Min <=1 or Max >= 22 and Min <=2 or Max >= 23 and Min <=3 or Max >= 24 and Min <=4:
+                    Min1 = Min+24
+                    phase_diff = Max-Min1
+                    if phase_diff <4:
+                        FatPhase =str( Min1/3.8216)
+                        output.write(D+','+S_Breast+','+Breast_Phase+','+S_Fat+','+tis+','+Fat_Phase+'\n')
+                    else:
+                        Fat_Phase = str(((Min1+Max)/2)/3.8216)
+                        output.write(D+','+S_Breast+','+Breast_Phase+','+S_Fat+','+tis+','+Fat_Phase+'\n')
+                else:
+                    phase_diff = Max-Min
+                    if phase_diff <4:
+                        FatPhase = str(Min/3.8216)
+                        output.write(D+','+S_Breast+','+Breast_Phase+','+S_Fat+','+tis+','+Fat_Phase+'\n')
+                    else:
+                        Fat_Phase = str(((Min+Max)/2)/3.8216)    
+                        output.write(D+','+S_Breast+','+Breast_Phase+','+S_Fat+','+tis+','+Fat_Phase+'\n')
+    #output.close()
             
             
 if __name__ == '__main__':
     phase_linearity()
+  
         
             
